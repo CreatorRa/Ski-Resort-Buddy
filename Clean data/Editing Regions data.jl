047638a1615d018aa123aaa1c413dbe96075b7b3
CreatorRa <73293604+CreatorRa@users.noreply.github.com>
2025-10-05 18:58:29 +0200

    Adding an extra file for clarity

---
+++ Clean data/Editing Regions data.jl
@@ -0,0 +1,36 @@
+using CSV
+using Dates
+using DataFrames
+
+filepath_regions = "c:\\Users\\Carter\\OneDrive\\Documents\\KLU\\KLU Studies\\Scientific Programming\\Clean Data\\more data\\ski-regions-data.csv"
+df_regions = CSV.read(filepath_regions, DataFrame)
+df_regions.Date = Date.(df_regions.Date)
+df_regions.Year = year.(df_regions.Date)
+df_regions.Month = month.(df_regions.Date)
+df_regions.Day = day.(df_regions.Date)
+
+select!(df_regions, Not(:Date))
+rename!(df_regions, "Snow Depth (cm)" => "Daily Snow Depth (cm)")
+
+sort!(df_regions, :Region)
+all_cols = names(df_regions)
+date_cols_to_move = ["Year", "Month", "Day"]
+other_cols = setdiff(all_cols, date_cols_to_move)
+country_index = findfirst(isequal("Country"), other_cols)
+new_order = vcat(
+    other_cols[1:country_index],      # Columns up to and including 'Country'
+    date_cols_to_move,                # Insert the date columns here
+    other_cols[country_index+1:end]   # Add the rest of the columns
+)
+select!(df_regions, new_order)
+
+insertcols!(df_regions, 11, :"Mean Snow Depth (cm)" => missing)
+insertcols!(df_regions, 12, :"Max Snow Depth (cm))" => missing)
+insertcols!(df_regions, 13, :"Days where AVG Temp < 0C" => missing)
+
+# --- Export the edited DataFrame to a new CSV file ---
+first(df_regions, 5)
+Output_filepath = "c:\\Users\\Carter\\OneDrive\\Documents\\KLU\\KLU Studies\\Scientific Programming\\Clean Data\\more data\\Regions edited.csv"
+CSV.write(output_filepath, df_regions)
+
+
